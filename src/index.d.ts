c127647f1355b2c4ac439ff43ca1d805c102d948
Sakthivel Govinthan <sakthiveltechit@gmail.com>
2025-03-07 13:36:19 +0530

    Add package.json

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+import { IconType } from "react-icons";
+
+export interface DateFilterProps {
+  startDate?: Date | null;
+  endDate?: Date | null;
+  onSelect?: (date: Date | null) => void;
+  onChoose?: (date: Date | null) => void;
+  selectedColor?: string;
+  icon?: IconType;
+}
+
+declare const DateFilter: (props: DateFilterProps) => ReactElement;
+
+export default DateFilter;
